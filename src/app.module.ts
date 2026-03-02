7d0799b60ea39f46341b81637699ab170deb0a51
henrynnamani <hoyx0101@gmail.com>
2026-03-02 12:00:12 +0100

    feat: working

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfessionModule } from './confession/confession.module';
 import { ReactionLogModule } from './reaction_log/reaction_log.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://graey:moOfff6PtqRNSuqr@cluster0.eh52mqv.mongodb.net/?appName=Cluster0'
     ),
+    ScheduleModule.forRoot(),
     ConfessionModule,
     ReactionLogModule
   ],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  @Cron(CronExpression.EVERY_MINUTE)
+  async pingServer() {
+    try {
+      await axios.get(`${process.env.APP_URL}/health`);
+      console.log('Server pinged successfully');
+    } catch (err) {
+      console.error('Ping failed', err.message);
+    }
   }
 }
